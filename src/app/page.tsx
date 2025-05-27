2bef3976cc8471eadb524d12002a57511d45069b
Meryam Rakkez <rakkezmeryam@gmail.com>
2025-05-27 17:31:01 +0100

    add navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
-import Hero from '@/components/Hero'
-import React from 'react'
+import Hero from "@/components/Hero";
+import Nav from "@/components/Nav";
+import React from "react";
 
 const Page = () => {
   return (
     <>
+      <Nav />
       <Hero />
     </>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx,mdx}",
+    "./app/**/*.{js,jsx,ts,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
