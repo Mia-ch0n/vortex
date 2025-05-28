630877f921e67a88c5f84c297f220c0acc1cd126
Meryam Rakkez <rakkezmeryam@gmail.com>
2025-05-28 14:12:03 +0100

    setup supa base client +api get endopoint

---
+++ src/app/api/games/route.js
@@ -0,0 +1,17 @@
+import { supabase } from '@/lib/supabaseClient'
+
+export async function GET() {
+  const { data, error } = await supabase.from('games').select('*')
+
+  if (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { 'Content-Type': 'application/json' },
+    })
+  }
+
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+  })
+}
