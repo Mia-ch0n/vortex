a61a9c143e1713fc5d7423ad3662368e72983e49
Meryam Rakkez <rakkezmeryam@gmail.com>
2025-05-29 01:43:16 +0100

    add get system requirements and search to games

--- src/app/api/games/route.js
+++ src/app/api/games/route.js
@@ -1,17 +1,26 @@
-import { supabase } from '@/lib/supabaseClient'
+import { supabase } from "@/lib/supabaseClient";
 
-export async function GET() {
-  const { data, error } = await supabase.from('games').select('*')
+export async function GET(request) {
+  const { searchParams } = new URL(request.url);
+  const search = searchParams.get("search");
+
+  let query = supabase.from("games").select("*, system_requirements(*)");
+
+  if (search) {
+    query = query.ilike("name", `%${search}%`);
+  }
+
+  const { data, error } = await query;
 
   if (error) {
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
-      headers: { 'Content-Type': 'application/json' },
-    })
+      headers: { "Content-Type": "application/json" },
+    });
   }
 
   return new Response(JSON.stringify(data), {
     status: 200,
-    headers: { 'Content-Type': 'application/json' },
-  })
+    headers: { "Content-Type": "application/json" },
+  });
 }
